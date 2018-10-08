a7818af4ed9acc806c94d57f91e3822e866cfca4
John Eckhart <johne@nearpod.com>
2018-10-07 22:54:42 -0400

    Update package-openvpn test suite to use latest version of Terratest

--- test/openvpn_test.go
+++ test/openvpn_test.go
@@ -1,27 +1,26 @@
 package test
 
 import (
-	"testing"
-	terralog "github.com/gruntwork-io/terratest/log"
-	"github.com/gruntwork-io/terratest/resources"
-	"github.com/gruntwork-io/terratest"
-	"github.com/gruntwork-io/terratest/packer"
 	"fmt"
-	"time"
-	"strconv"
-	"github.com/gruntwork-io/terratest/git"
-	"github.com/gruntwork-io/terratest/aws"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/git"
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/packer"
+	"github.com/gruntwork-io/terratest/modules/retry"
+	"github.com/gruntwork-io/terratest/modules/ssh"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
-	"github.com/gruntwork-io/terratest/ssh"
+	"strconv"
 	"strings"
-	"log"
-	"github.com/gruntwork-io/terratest/util"
+	"testing"
+	"time"
 )
 
 type suite struct {
-	logger             *log.Logger
-	resourceCollection *terratest.RandomResourceCollection
-	terratestOptions   *terratest.TerratestOptions
+	terraformOptions   *terraform.Options
+	keyPair            *aws.Ec2Keypair
+	accountId          string
+	region             string
 	ipAddress          string
 	host               ssh.Host
 	output             string
@@ -33,73 +32,61 @@ func TestOpenVpnInitializationSuite(t *testing.T) {
 		ipAddress:"",
 	}
 
-	testSuite.logger = terralog.NewLogger("TestOpenVpnSuite")
-	testSuite.resourceCollection = resources.CreateBaseRandomResourceCollection(t, "ap-northeast-1")
+	testSuite.region = aws.GetRandomRegion(t, []string{"ca-central-1"}, nil)
+	testSuite.accountId = aws.GetAccountId(t)
 
 	//Build the AMI with packer
-	packerOptions := packer.PackerOptions{
+	packerOptions := &packer.Options{
 		Template: "../examples/packer/openvpn-server-ubuntu1604.json",
 		Vars: map[string]string{
-			"aws_region": testSuite.resourceCollection.AwsRegion,
-			"aws_account_id": testSuite.resourceCollection.AccountId,
+			"aws_region": testSuite.region,
+			"aws_account_id": testSuite.accountId,
 			"package_openvpn_branch": git.GetCurrentBranchName(t),
 			"active_git_branch": git.GetCurrentBranchName(t),
 		},
 	}
 
-	amiId, err := packer.BuildAmi(packerOptions, testSuite.logger)
-	if err != nil || len(amiId) == 0 {
-		t.Fatalf("Failed to build AMI due to error: %s", err.Error())
-	}
+	amiId := packer.BuildArtifact(t, packerOptions)
 
-	vpc, err := testSuite.resourceCollection.GetDefaultVpc()
-	if err != nil {
-		t.Fatalf("Failed to get default VPC: %s\n", err.Error())
-	}
+	vpc := aws.GetDefaultVpc(t, testSuite.region)
 
 	if len(vpc.Subnets) == 0 {
 		t.Fatalf("Default vpc %s contained no subnets", vpc.Id)
 	}
 
+	testSuite.keyPair = aws.CreateAndImportEC2KeyPair(t, testSuite.region, getRandomizedString("tst-openvpn-key"))
+
 	//Setup terratest
-	testSuite.terratestOptions = resources.CreateBaseTerratestOptions(t, "OpenVpnTestTerraform", "../examples/openvpn-host", testSuite.resourceCollection)
-
-	testSuite.terratestOptions.Vars["name"] = getRandomizedString("tst-openvpn-host")
-	testSuite.terratestOptions.Vars["backup_bucket_name"] = getRandomizedString("tst-openvpn")
-	testSuite.terratestOptions.Vars["request_queue_name"] = getRandomizedString("tst-openvpn-requests")
-	testSuite.terratestOptions.Vars["revocation_queue_name"] = getRandomizedString("tst-openvpn-revocations")
-	testSuite.terratestOptions.Vars["ami"] = amiId
-	testSuite.terratestOptions.Vars["keypair_name"] = testSuite.resourceCollection.KeyPair.Name
-
-	_, err = terratest.Apply(testSuite.terratestOptions)
-	if err != nil {
-		t.Fatalf("Unexpected error when applying terraform templates: %v", err)
+	testSuite.terraformOptions = &terraform.Options{
+		TerraformDir: "../examples/openvpn-host",
+		Vars: map[string]interface{}{
+			"name": getRandomizedString("tst-openvpn-host"),
+			"backup_bucket_name": getRandomizedString("tst-openvpn"),
+			"request_queue_name": getRandomizedString("tst-openvpn-requests"),
+			"revocation_queue_name": getRandomizedString("tst-openvpn-revocations"),
+			"ami": amiId,
+			"keypair_name": testSuite.keyPair.Name,
+			"aws_account_id": testSuite.accountId,
+			"aws_region": testSuite.region,
+		},
 	}
+
+
+	terraform.InitAndApply(t, testSuite.terraformOptions)
 
 	defer func() {
-		ec2Client, err := aws.CreateEC2Client(testSuite.resourceCollection.AwsRegion)
-		if err != nil {
-			t.Fatal("Error creating EC2 Client")
-		}
+		terraform.Destroy(t, testSuite.terraformOptions)
 
-		_, err = terratest.Destroy(testSuite.terratestOptions, testSuite.resourceCollection)
-		if err != nil {
-			t.Fatal("Terraform Destroy failed")
-		}
-
-		aws.RemoveAmi(ec2Client, amiId)
-		testSuite.logger.Println("TearDownSuite Complete")
+		aws.DeleteAmi(t, testSuite.region, amiId)
+		logger.Log(t, "TearDownSuite Complete")
 	}()
 
-	testSuite.ipAddress, err = terratest.Output(testSuite.terratestOptions, "openvpn_host_public_ip")
-	if err != nil {
-		t.Fatalf("An error occurred retreiving terraform output - %s", err.Error())
-	}
+	testSuite.ipAddress = terraform.Output(t, testSuite.terraformOptions, "openvpn_host_public_ip")
 
 	waitUntilSshAvailable(t, &testSuite)
 	waitUntilOpenVpnInitComplete(t, &testSuite)
 
-	testSuite.logger.Println("SetupSuite Complete, Running Tests")
+	t.Log("SetupSuite Complete, Running Tests")
 
 	t.Run("openvpn tests", func(t *testing.T) {
 		t.Run("running testOpenVpnIsRunning", wrapTestCase(testOpenVpnIsRunning, &testSuite))
@@ -125,43 +112,36 @@ func waitUntilSshAvailable(t *testing.T, testSuite *suite) {
 	testSuite.host = ssh.Host{
 		Hostname: testSuite.ipAddress,
 		SshUserName: "ubuntu",
-		SshKeyPair: testSuite.resourceCollection.KeyPair,
+		SshKeyPair: testSuite.keyPair.KeyPair,
 	}
 
-	_, err := util.DoWithRetry(
+	retry.DoWithRetry(
+		t,
 		fmt.Sprintf("SSH to public host %s", testSuite.ipAddress),
 		20,
 		30 * time.Second,
-		testSuite.logger,
 		func() (string, error) {
-			return "", ssh.CheckSshConnection(testSuite.host, testSuite.logger)
+			return "", ssh.CheckSshConnectionE(t, testSuite.host)
 		},
 	)
 
-	if err != nil {
-		t.Fatalf("Failed to SSH to host at %s :%s\n", testSuite.ipAddress, err.Error())
-	}
 }
 
 func waitUntilOpenVpnInitComplete(t *testing.T, testSuite *suite) {
-	_, err := util.DoWithRetry(
+	retry.DoWithRetry(
+		t,
 		fmt.Sprintf("Waiting for OpenVPN initialization to complete"),
 		75,
 		30 * time.Second,
-		testSuite.logger,
 		func() (string, error) {
 			return "", initComplete(t, testSuite)
 		},
 	)
-
-	if err != nil {
-		t.Fatal("OpenVPN initilization failed to complete")
-	}
 }
 
 func initComplete(t *testing.T, testSuite *suite) (error) {
 	command := "sudo ls /etc/openvpn/openvpn-init-complete"
-	output, err := ssh.CheckSshCommand(testSuite.host, command, testSuite.logger)
+	output, err := ssh.CheckSshCommandE(t, testSuite.host, command)
 
 	if strings.Contains(output, "such file or directory") {
 		return fmt.Errorf("OpenVPN initialization not yet complete")
@@ -174,24 +154,9 @@ func initComplete(t *testing.T, testSuite *suite) (error) {
 	return nil
 }
 
-func getInstanceId(t *testing.T, testSuite *suite) string {
-	//Get the instance id of the running instance
-	command := "curl --silent http://169.254.169.254/latest/meta-data/instance-id"
-	output, err := ssh.CheckSshCommand(testSuite.host, command, testSuite.logger)
-	if err != nil {
-		t.Fatalf("Unable to get instance id :\n", err.Error())
-	}
-
-	return output
-}
-
 func testOpenVpnIsRunning(t *testing.T, testSuite *suite) {
 	commandToTest := "sudo ps -ef|grep openvpn"
-	var err error
-	testSuite.output, err = ssh.CheckSshCommand(testSuite.host, commandToTest, testSuite.logger)
-	if err != nil {
-		t.Fatalf("Failed to SSH to AMI Builder at %s and execute command :%s\n", testSuite.ipAddress, err.Error())
-	}
+	testSuite.output = ssh.CheckSshCommand(t, testSuite.host, commandToTest)
 
 	// It will be convenient to see the full command output directly in logs. This will show only when there's a test failure.
 	t.Logf("Result of running \"%s\"\n", commandToTest)
@@ -202,11 +167,7 @@ func testOpenVpnIsRunning(t *testing.T, testSuite *suite) {
 
 func testOpenVpnAdminProcessRequestsIsRunning(t *testing.T, testSuite *suite) {
 	commandToTest := "sudo ps -ef|grep openvpn"
-	var err error
-	testSuite.output, err = ssh.CheckSshCommand(testSuite.host, commandToTest, testSuite.logger)
-	if err != nil {
-		t.Fatalf("Failed to SSH to EC2 Instance at %s and execute command :%s\n", testSuite.ipAddress, err.Error())
-	}
+	testSuite.output = ssh.CheckSshCommand(t, testSuite.host, commandToTest)
 
 	// It will be convenient to see the full command output directly in logs. This will show only when there's a test failure.
 	t.Logf("Result of running \"%s\"\n", commandToTest)
@@ -218,7 +179,7 @@ func testOpenVpnAdminProcessRequestsIsRunning(t *testing.T, testSuite *suite) {
 func testOpenVpnAdminProcessRevokesIsRunning(t *testing.T, testSuite *suite) {
 	commandToTest := "sudo ps -ef|grep openvpn"
 	var err error
-	testSuite.output, err = ssh.CheckSshCommand(testSuite.host, commandToTest, testSuite.logger)
+	testSuite.output = ssh.CheckSshCommand(t, testSuite.host, commandToTest)
 	if err != nil {
 		t.Fatalf("Failed to SSH to AMI Builder at %s and execute command :%s\n", testSuite.ipAddress, err.Error())
 	}
@@ -232,11 +193,7 @@ func testOpenVpnAdminProcessRevokesIsRunning(t *testing.T, testSuite *suite) {
 
 func testCronJobExists(t *testing.T, testSuite *suite) {
 	commandToTest := "sudo cat /etc/cron.hourly/backup-openvpn-pki"
-	var err error
-	testSuite.output, err = ssh.CheckSshCommand(testSuite.host, commandToTest, testSuite.logger)
-	if err != nil {
-		t.Fatalf("Failed to SSH to AMI Builder at %s and execute command :%s\n", testSuite.ipAddress, err.Error())
-	}
+	testSuite.output = ssh.CheckSshCommand(t, testSuite.host, commandToTest)
 
 	// It will be convenient to see the full command output directly in logs. This will show only when there's a test failure.
 	t.Logf("Result of running \"%s\"\n", commandToTest)
@@ -247,11 +204,7 @@ func testCronJobExists(t *testing.T, testSuite *suite) {
 
 func testCrlExpirationDateUpdated(t *testing.T, testSuite *suite) {
 	commandToTest := "sudo cat /etc/openvpn-ca/openssl-1.0.0.cnf"
-	var err error
-	testSuite.output, err = ssh.CheckSshCommand(testSuite.host, commandToTest, testSuite.logger)
-	if err != nil {
-		t.Fatalf("Failed to SSH to AMI Builder at %s and execute command :%s\n", testSuite.ipAddress, err.Error())
-	}
+	testSuite.output = ssh.CheckSshCommand(t, testSuite.host, commandToTest)
 
 	// It will be convenient to see the full command output directly in logs. This will show only when there's a test failure.
 	t.Logf("Result of running \"%s\"\n", commandToTest)
