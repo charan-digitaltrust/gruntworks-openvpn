e709e729ccda34535bbc8228bfa06353bc938b3f
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-06-17 20:11:38 -0700

    Update to tf12 syntax

--- examples/openvpn-host/vars.tf
+++ examples/openvpn-host/vars.tf
@@ -13,38 +13,46 @@
 
 variable "aws_region" {
   description = "The AWS region in which all resources will be created"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "aws_account_id" {
   description = "The AWS account ID where the OpenVPN Server will be created. Note that all IAM Users who receive OpenVPN access must also reside in this AWS account."
+  type        = string
 }
 
 variable "keypair_name" {
   description = "The AWS EC2 Keypair name for root access to the OpenVPN host."
-  default     = ""
+  type        = string
+  default     = null
 }
 
 variable "backup_bucket_name" {
   description = "The name of the s3 bucket that will hold the backup of the PKI for the OpenVPN server"
+  type        = string
   default     = "openvpn-backups"
 }
 
 variable "request_queue_name" {
   description = "The name of the sqs queue that will be used to receive new certificate requests. Note that the queue name will be automatically prefixed with 'openvpn-requests-'."
+  type        = string
   default     = "example"
 }
 
 variable "revocation_queue_name" {
   description = "The name of the sqs queue that will be used to receive certificate revocation requests. Note that the queue name will be automatically prefixed with 'openvpn-revocations-'."
+  type        = string
   default     = "example"
 }
 
 variable "name" {
   description = "The name of the openvpn host"
+  type        = string
   default     = "openvpn-host"
 }
 
 variable "ami_id" {
   description = "The ID of the AMI to run. Should be an AMI built from the Packer template in /examples/packer/build.json"
+  type        = string
 }
--- modules/openvpn-server/outputs.tf
+++ modules/openvpn-server/outputs.tf
@@ -1,60 +1,59 @@
 output "autoscaling_group_id" {
-  value = "${aws_autoscaling_group.openvpn.id}"
+  value = aws_autoscaling_group.openvpn.id
 }
 
 output "public_ip" {
-  value = "${aws_eip.openvpn.public_ip}"
+  value = aws_eip.openvpn.public_ip
 }
 
 output "private_ip" {
-  value = "${aws_eip.openvpn.private_ip}"
+  value = aws_eip.openvpn.private_ip
 }
 
 output "elastic_ip" {
-  value = "${aws_eip.openvpn.id}"
+  value = aws_eip.openvpn.id
 }
 
 output "security_group_id" {
-  value = "${aws_security_group.openvpn.id}"
+  value = aws_security_group.openvpn.id
 }
 
 output "iam_role_id" {
-  value = "${aws_iam_role.openvpn.id}"
+  value = aws_iam_role.openvpn.id
 }
 
 output "client_request_queue" {
-  value = "${aws_sqs_queue.client-request-queue.id}"
+  value = aws_sqs_queue.client-request-queue.id
 }
 
 output "client_revocation_queue" {
-  value = "${aws_sqs_queue.client-revocation-queue.id}"
+  value = aws_sqs_queue.client-revocation-queue.id
 }
 
 output "backup_bucket_name" {
-  value = "${lower(var.backup_bucket_name)}"
+  value = lower(var.backup_bucket_name)
 }
 
 output "allow_certificate_requests_for_external_accounts_iam_role_id" {
-  value = "${aws_iam_role.allow_certificate_requests_for_external_accounts.*.id}"
+  value = aws_iam_role.allow_certificate_requests_for_external_accounts.*.id[0]
 }
 
 output "allow_certificate_requests_for_external_accounts_iam_role_arn" {
-  value = "${aws_iam_role.allow_certificate_requests_for_external_accounts.*.arn}"
+  value = aws_iam_role.allow_certificate_requests_for_external_accounts.*.arn[0]
 }
 
 output "allow_certificate_revocations_for_external_accounts_iam_role_id" {
-  value = "${aws_iam_role.allow_certificate_revocations_for_external_accounts.*.id}"
+  value = aws_iam_role.allow_certificate_revocations_for_external_accounts.*.id[0]
 }
 
 output "allow_certificate_revocations_for_external_accounts_iam_role_arn" {
-  value = "${aws_iam_role.allow_certificate_revocations_for_external_accounts.*.arn}"
+  value = aws_iam_role.allow_certificate_revocations_for_external_accounts.*.arn[0]
 }
 
 output "openvpn_users_group_name" {
-  value = "${aws_iam_group.openvpn-users.name}"
+  value = aws_iam_group.openvpn-users.name
 }
 
 output "openvpn_admins_group_name" {
-  value = "${aws_iam_group.openvpn-admins.name}"
+  value = aws_iam_group.openvpn-admins.name
 }
-
