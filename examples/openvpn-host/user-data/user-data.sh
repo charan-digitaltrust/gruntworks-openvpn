f2ca28074f5b430427299117a4df2271138dbec0
Jesse Bye <jbye@riskalyze.com>
2019-07-09 11:22:16 -0700

    Support for duo plugin

--- examples/openvpn-host/user-data/user-data.sh
+++ examples/openvpn-host/user-data/user-data.sh
@@ -42,6 +42,9 @@ sudo init-openvpn  \
  --ca-expiration-days "${ca_expiration_days}" \
  --cert-expiration-days "${cert_expiration_days}" \
  --vpn-subnet "${vpn_subnet}" \
+ --duo-ikey "${duo_ikey}" \
+ --duo-skey "${duo_skey}" \
+ --duo-host "${duo_host}" \
  ${routes}
 
 echo 'Starting Certificate Request/Revoke Daemons...'
