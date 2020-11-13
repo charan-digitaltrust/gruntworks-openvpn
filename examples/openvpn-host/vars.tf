51a49d572c95d323f447cf389450811a00731b1e
Ina Stoyanova <ina@gruntwork.io>
2020-11-13 19:21:17 +0000

    adding some temporary test variable values after building packer AMI

--- examples/openvpn-host/vars.tf
+++ examples/openvpn-host/vars.tf
@@ -14,47 +14,49 @@
 variable "aws_account_id" {
   description = "The AWS account ID where the OpenVPN Server will be created. Note that all IAM Users who receive OpenVPN access must also reside in this AWS account."
   type        = string
+  default     = "738755648600"
 }
 
 variable "ami_id" {
   description = "The ID of the AMI to run. Should be an AMI built from the Packer template in /examples/packer/build.json"
   type        = string
+  default     = "ami-05b1793b1053009ff"
 }
 
 variable "aws_region" {
   description = "The AWS region in which all resources will be created"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-west-2"
 }
 
 variable "keypair_name" {
   description = "The AWS EC2 Keypair name for root access to the OpenVPN host."
   type        = string
-  default     = null
+  default     = "test-ina"
 }
 
 variable "backup_bucket_name" {
   description = "The name of the s3 bucket that will hold the backup of the PKI for the OpenVPN server"
   type        = string
-  default     = "openvpn-backups"
+  default     = "test-ina-openvpn-backups"
 }
 
 variable "request_queue_name" {
   description = "The name of the sqs queue that will be used to receive new certificate requests. Note that the queue name will be automatically prefixed with 'openvpn-requests-'."
   type        = string
-  default     = "example"
+  default     = "test-ina-example"
 }
 
 variable "revocation_queue_name" {
   description = "The name of the sqs queue that will be used to receive certificate revocation requests. Note that the queue name will be automatically prefixed with 'openvpn-revocations-'."
   type        = string
-  default     = "example"
+  default     = "test-ina-example"
 }
 
 variable "name" {
   description = "The name of the openvpn host"
   type        = string
-  default     = "openvpn-host"
+  default     = "test-ina-openvpn-host"
 }
 
 variable "instance_type" {
